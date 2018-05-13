3b3c6b72f5d0a6ccb2300630e4461d5eafce80b7
shanthi <shanthivaraganti1@gmail.com>
2018-05-13 12:08:03 -0400

    Created first project

---
+++ HelloWorldProject/src/HelloWorldClass.java
@@ -0,0 +1,10 @@
+//class declaration
+public class HelloWorldClass
+{
+	//Entry point for the program
+	public static void main(String args[])
+	{
+		//prints"Hello World" on to the  console
+		System.out.println("Hello World shanthi!");
+	}
+}
